fc81a90a5d1c25477620cdd93053930c65ae8223
mikaelalundstrom <mickismickis@hotmail.se>
2024-08-20 16:57:12 +0200

    functionality to add, remove, and change status of list item

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const handleSubmit = (event: any) => {
+    // to not refresh page
+    event.preventDefault();
+    // set the tasks to the current tasks + new task
+    setTasks((tasks) => [...tasks, newTask]);
+    // to empty input field
+    event.target.reset();
+  };
+
+  // change individual h3 class to done
+  const updateStatus = (event: any) => {
+    event.target.parentElement.children[0].className = "done";
+  };
+
+  // filter out clicked task for tasks list, set tasks again
+  const handleRemove = (i: number) => {
+    const updatedTasks = tasks.filter((task, index) => index !== i);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <>
+      <h1>To-do List</h1>
+      <h2>Write a New Task:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => {
+            setNewTask(e.target.value);
+          }}
+          type="text"
+          placeholder="Enter task..."
+          required
+        />
+        <button>Add</button>
+      </form>
+
+      <h2>Your Tasks:</h2>
+      <ul>
+        {tasks.map((task, i) => (
+          <li key={i}>
+            <h3 className="todo">{task}</h3>
+            <button onClick={updateStatus}>Done</button>
+            <button
+              onClick={() => {
+                handleRemove(i);
+              }}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
